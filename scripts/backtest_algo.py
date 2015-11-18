d300e65e3a980533736260b9be6ae716077d9b80
vergl4s <luis@teix.co>
2015-11-18 22:00:46 +1100

    improvements to backtest_algo

--- scripts/backtest_algo.py
+++ scripts/backtest_algo.py
@@ -5,50 +5,36 @@ import imp
 import logging
 import os
 import sys
-
 import botcoin
 
 
-def load_script(filename=None):
+def load_script(filename, datadir, graph=False, all_trades=False, verbose=False):
 
-    parser = argparse.ArgumentParser(description='Botcoin script execution.')
-    parser.add_argument('-f', '--file', required=False, nargs='?', help='file with strategy scripts')
-    parser.add_argument('-g', '--graph', action='store_true', help='graph equity curve')
-    parser.add_argument('-a', '--all_trades', action='store_true', help='print all_trades dataframe')
-    parser.add_argument('-d', '--debug', action='store_true', help='debugging (very verbose, be careful)')
-    args = parser.parse_args()
-
-    if args.debug:
+    if verbose:
         botcoin.settings.VERBOSITY = 10
     logging.basicConfig(format=botcoin.settings.LOG_FORMAT, level=botcoin.settings.VERBOSITY)
 
-
-    filename = args.file or filename
-    if filename:
-        directory, file_to_load = os.path.split(os.path.abspath(filename))
-        sys.path.append(directory)
+    directory, file_to_load = os.path.split(os.path.abspath(filename))
+    sys.path.append(directory)
+    try:
         strategy_module = __import__(file_to_load.split('.')[0])
-    else:
-        logging.critical('No strategy file identified')
-        sys.exit()
-    #     try:
-    #         import custom as strategy_module
-    #     except ImportError:
-    #         logging.critical("No strategy found")
-    #         sys.exit()
+    except ImportError:
+        pass
 
+    if 'strategy_module' not in locals():
+        raise ValueError('Could not understand your strategy file')
 
-    data_dir = os.path.join(os.getcwd(),'data/')
+    datadir = os.path.expanduser(datadir)
 
     # Run backtest
-    backtest = botcoin.Backtest(_find_strategies(strategy_module), data_dir)
+    backtest = botcoin.Backtest(_find_strategies(strategy_module), datadir)
 
     print(backtest.results)
 
-    if args.all_trades:
+    if all_trades:
         backtest.print_all_trades()
 
-    if args.graph:
+    if graph:
         backtest.plot_results()
 
     backtest.strategy_finishing_methods()
@@ -80,10 +66,20 @@ def _find_strategies(module):
 
     raise ValueError('Could not understand your strategy script')
 
+def main():
+    parser = argparse.ArgumentParser(description='Botcoin script execution.')
+    parser.add_argument(dest='algo_file', nargs='+', help='file with strategy scripts')
+    parser.add_argument('-d', '--datadir', default=os.path.join(os.getcwd(),'data/'), required=False, nargs='?', help='data directory containing ohlc csvs (default is ./data/)')
+    parser.add_argument('-g', '--graph', action='store_true', help='graph equity curve')
+    parser.add_argument('-a', '--all_trades', action='store_true', help='print all_trades dataframe')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose (very chatty, be careful)')
+    args = parser.parse_args()
+
+    load_script(args.algo_file[0], args.datadir, graph=args.graph, all_trades=args.all_trades, verbose=args.verbose)
 
 if __name__ == '__main__':
     try:
-        load_script()
+        main()
     except KeyboardInterrupt:
         logging.critical("Execution stopped")
         sys.exit()
